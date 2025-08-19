d1b09facc53410a0d05de39a7301ff16369fb755
Maksym Dombrov <maksymdombrov@gmail.com>
2025-08-19 21:54:59 +0300

    notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,14 +1,32 @@
-import { useRouter } from 'expo-router';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { theme } from '../../theme';
+import { registerForPushNotificationsAsync } from '../../utils/registerForPushNotifications';
+import * as Device from 'expo-device';
 
 export default function CounterScreen() {
-  const router = useRouter();
+  const scheduleNotification = async () => {
+    const result = await registerForPushNotificationsAsync();
+    if (result === 'granted') {
+      // Permission granted, you can schedule a notification here if needed
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          'Unable to schedule notification',
+          'Enable the notification permission for Expo Go in settings'
+        );
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        onPress={() => router.navigate('/idea')}
-      ></TouchableOpacity>
-      <Text style={styles.text}>Counter</Text>
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={scheduleNotification}
+      >
+        <Text style={styles.buttonText}>Request Permission</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -20,7 +38,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff'
   },
-  text: {
-    fontSize: 24
+  buttonText: {
+    fontSize: 24,
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 1
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6
   }
 });
